27dbacee178c0963d525b01a1ddb19006f14838f
Tomás Vergara <tomvergara@uc.cl>
2022-04-22 23:20:35 -0400

    feat(): add initial behavior

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+export class Retry extends Error {}
+
+export async function checkpoint(
+  maxRetries: number,
+  func: () => void | Promise<void>
+) {
+  for (let i = 0; i <= maxRetries; i++) {
+    try {
+      await func();
+      return;
+    } catch (error) {
+      if (error instanceof Retry) {
+        continue;
+      }
+      throw error;
+    }
+  }
+
+  throw new Error(`checkpoint failed after ${maxRetries} retries`);
+}
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,69 @@
-import { myPackage } from '../src';
+import { checkpoint, Retry } from '../src';
 
 describe('index', () => {
-  describe('myPackage', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello';
+  describe('checkpoint', () => {
+    describe('when called with a function which always fails', () => {
+      it('should end up raising an error', async () => {
+        const result = await checkpoint(1, () => {
+          throw new Retry('failure');
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        }).catch(error => error);
 
-      const result = myPackage(message);
+        expect(result instanceof Error).toBe(true);
+      });
+    });
 
-      expect(result).toMatch(message);
+    describe('when called with a function which always succeeds', () => {
+      it('should end up returning the result', async () => {
+        const result = await checkpoint(1, () => {
+          'it succeeds';
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        }).catch(error => error);
+
+        expect(result instanceof Error).toBe(false);
+      });
+    });
+
+    describe('when called with a function which fails before succeeding', () => {
+      const functionSuccesses = [false, true];
+
+      it('should not raise an error', async () => {
+        let i = 0;
+        const result = await checkpoint(1, () => {
+          if (functionSuccesses[i]) return;
+
+          i += 1;
+          throw new Retry('failure');
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        }).catch(error => error);
+
+        expect(result instanceof Error).toBe(false);
+      });
+
+      it('should call the function as many times as it fails', async () => {
+        let i = 0;
+        await checkpoint(1, () => {
+          if (functionSuccesses[i]) return;
+
+          i += 1;
+          throw new Retry('failure');
+        });
+
+        expect(i).toBe(1);
+      });
+    });
+
+    describe('when function fails unexpectedly', () => {
+      it('should raise an error', async () => {
+        const result = await checkpoint(1, () => {
+          throw new Error('some unexpected error');
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        }).catch(error => error);
+
+        expect(result instanceof Error).toBe(true);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        expect(result.message).toBe('some unexpected error');
+      });
     });
   });
 });
